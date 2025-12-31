b84d626ff6f84e1868702b8a8c151b6d8029f694
irshadCI001 <irshad.ibrahim.7770@gmail.com>
2025-12-31 13:47:57 +0530

    OOPs Test

---
+++ .NetCore/Machine Test (OOPs)/BankingManagementSystem/BankingManagementSystem/SavingsAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankingManagementSystem
+{
+    internal class SavingsAccount : Account
+    {
+        public override void AccountInfo()
+        {
+            Console.WriteLine("Savings Account Information:");
+            Console.WriteLine("----------------------------");
+
+            Console.WriteLine($"Savings Account Holder: {AccountHolder}\nBalance: {Balance}rs");
+            Console.WriteLine();
+        }
+        public override void CalculateInterest()
+        {
+            
+            decimal interestRate = 0.05m;
+            decimal interest = Balance * interestRate;
+            Balance += interest;
+            Console.WriteLine($"Interest of {interest}rs applied. New balance: {Balance}rs");
+        }
+
+    }
+}
