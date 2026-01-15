6ad5a9594155b470e14fede7e312a9781726418b
manumj19939019 <manumj1993@gmial.com>
2026-01-15 15:39:55 +0530

    added exercise

---
+++ .NetCore/Machine Test (Exceptions)/Manu/JobProvider/Exception.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace JobProvider
+{
+    internal class DuplicateUserException : Exception
+    {
+        public DuplicateUserException(string message) : base(message) { }
+    }
+
+    internal class InvalidCredentialsException : Exception
+    {
+        public InvalidCredentialsException(string message) : base(message) { }
+    }
+
+    internal class JobNotFoundException : Exception
+    {
+        public JobNotFoundException(string message) : base(message) { }
+    }
+
+    internal class InvalidInputException : Exception
+    {
+        public InvalidInputException(string message) : base(message) { }
+    }
+}
---
+++ .NetCore/Machine Test (Exceptions)/Manu/JobProvider/User.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace JobProvider
+{
+    internal enum UserRole
+    {
+        JobSeeker,
+        JobProvider
+    }
+
+    internal class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; } 
+        public string Password { get; set; }
+        public UserRole Role { get; set; }
+
+        public User(string firstName, string lastName, string email, string phone, string password, UserRole role)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                throw new InvalidInputException("Email cannot be empty");
+            }
+            else if (!email.Contains("@"))
+            {
+                throw new InvalidInputException("Invalid email format");
+            }
+
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email.ToLower();
+            Phone = phone;
+            Password = password;
+            Role = role;
+        }
+
+        public bool Authenticate(string password)
+        {
+            return Password == password;
+        }
+
+        public string GetProfile()
+        {
+            return $"Name: {FirstName} {LastName}\nEmail: {Email}\nPhone: {Phone}\nRole: {Role}";
+        }
+    }
+}
