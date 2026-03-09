1bb0830df8a42ab4cc740d7501f43cffa748d3c9
irshadCI001 <irshad.ibrahim.7770@gmail.com>
2026-03-09 12:18:45 +0530

    machine test

---
+++ .NetCore/Chapter 8/Machine Test/Irshad/TaskManagerApp/TaskManagerApp/Model/TaskList.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskManagerApp.Model
+{
+    public class TaskList
+    {
+        public int Id {get; set; }
+        public string? Title {get; set; }
+        public DateTime DueDate {get; set; }= DateTime.Now;
+        public string? Description {get; set; }
+        public bool IsCompleted {get; set; }
+    }
+}
