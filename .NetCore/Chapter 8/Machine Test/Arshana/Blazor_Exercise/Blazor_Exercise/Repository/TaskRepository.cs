ee98ebc5ec76c9756e668d1c1d7da77bbe3357aa
Arshana Thasni TM <148333016+arshana-thasni-tm@users.noreply.github.com>
2026-03-09 22:20:49 +0530

    adding

---
+++ .NetCore/Chapter 8/Machine Test/Arshana/Blazor_Exercise/Blazor_Exercise/Repository/TaskRepository.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using Blazor_Exercise.Interface;
+using Blazor_Exercise.Model;
+
+
+namespace Blazor_Exercise.Repository
+{
+    public class TaskRepository : ITaskRepository
+    {
+        private readonly List<TaskItem> tasks = new();
+        private int nextId = 1;
+
+        public Task<List<TaskItem>> GetTasksAsync()
+        {
+            return Task.FromResult(tasks);
+        }
+
+        public Task AddTaskAsync(TaskItem task)
+        {
+            task.Id = nextId++;
+            task.IsCompleted = false;
+
+            tasks.Add(task);
+
+            return Task.CompletedTask;
+        }
+
+        public Task DeleteTaskAsync(int id)
+        {
+            var task = tasks.FirstOrDefault(x => x.Id == id);
+
+            if (task != null)
+                tasks.Remove(task);
+
+            return Task.CompletedTask;
+        }
+
+        public Task MarkCompleteAsync(int id)
+        {
+            var task = tasks.FirstOrDefault(x => x.Id == id);
+
+            if (task != null)
+                task.IsCompleted = true;
+
+            return Task.CompletedTask;
+        }
+    }
+}
