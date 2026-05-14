0381bc2ac62ec5a679384bcfe0d3f33fd99090a5
Edwin9762 <edwinbaby9762@gmail.com>
2026-05-14 13:27:54 +0530

    add

--- Project/JOB PORTAL SYSTEM/Domain/Services/Auth/DTO/SignupRequestDTO.cs
+++ Project/JOB PORTAL SYSTEM/Domain/Services/Auth/DTO/SignupRequestDTO.cs
@@ -6,7 +6,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Domain.Services.Auth.DTO
+
+namespace Domain.Services.Job_Provider.Signup.Dto
 {
     public class SignupRequestDTO
     {
