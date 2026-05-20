98dfb1b032df719c3a571d2d60acbcf2f74ef8cf
manumj19939019 <manumj1993@gmial.com>
2026-05-20 13:10:08 +0530

    updated savedjobs controller

--- Project/JOB PORTAL SYSTEM/JOB PORTAL SYSTEM/Api/Admin/AdminController.cs
+++ Project/JOB PORTAL SYSTEM/JOB PORTAL SYSTEM/Api/Admin/AdminController.cs
@@ -1,77 +1,30 @@
-﻿using Domain.Data;
+﻿using Domain.Services.Admin.CompanyVerification.Interface;
 using Domain.Services.Admin.Dto;
 using Domain.Services.Admin.Interface;
 using Domain.Services.Jobs.Interfaces;
-using JOB_PORTAL_SYSTEM.Api.Admin.RequestObjects;
+using JOB_PORTAL_SYSTEM.Api.ADMIN.RequestObjects;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace JOB_PORTAL_SYSTEM.Api.Admin
+namespace JOB_PORTAL_SYSTEM.Api.ADMIN
 {
-    [Tags("02-Admin")]
     [Route("api/[controller]")]
     [ApiController]
-    public class AdminController : ControllerBase
+    public class PlatFormAdminController : ControllerBase
     {
-
-        private readonly IUserService _userService;
-        private readonly ILocationRepository _locationRepository;
-        private readonly IQualificationService _qualificationService;
-        private readonly AppDbContext _context;
         private readonly Domain.Services.Admin.CompanyVerification.Interface.IAdminService _companyService;
         private readonly IJobService _jobService;
         private readonly ISkillService _skillService;
-        public AdminController(IUserService userService, ILocationRepository locationRepository, IQualificationService qualificationService, AppDbContext context, Domain.Services.Admin.CompanyVerification.Interface.IAdminService companyService, IJobService jobService, ISkillService skillService)
-        {
 
-            _userService = userService;
-            _locationRepository = locationRepository;
-            _qualificationService = qualificationService;
-            _context = context;
+        public PlatFormAdminController(Domain.Services.Admin.CompanyVerification.Interface.IAdminService companyService, IJobService jobService, ISkillService skillService)
+        {
             _companyService = companyService;
             _jobService = jobService;
             _skillService = skillService;
         }
 
-
-        [Authorize(Roles = "Admin")]
-        [HttpGet("users")]
-        public async Task<IActionResult> GetAllUsers()
-        {
-            try
-            {
-                var users = await _userService.GetAllUsersAsync();
-
-                return Ok(users);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
-        }
-
-        [Authorize(Roles = "Admin")]
-        [HttpGet("users/{id}")]
-        public async Task<IActionResult> GetUserById(Guid id)
-        {
-            try
-            {
-                var user = await _userService.GetUserByIdAsync(id);
-
-                if (user == null)
-                {
-                    return NotFound("User Not Found");
-                }
-
-                return Ok(user);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
-        }
-
-        [Authorize(Roles = "Admin")]
+        //[Authorize(Roles = "Admin")]
         [HttpPut("company-verification/{Id}/verify")]
         public async Task<IActionResult> VerifyCompany(Guid Id)
         {
@@ -91,18 +44,18 @@ namespace JOB_PORTAL_SYSTEM.Api.Admin
         }
 
         [HttpGet("profiles/{id}")]
-        public async Task<IActionResult> GetCompanyProfile(Guid id)
-        {
-            try
-            {
-                var profile = await _companyService.GetCompanyProfileByIdAsync(id);
+                public async Task<IActionResult> GetCompanyProfile(Guid id)
+                {
+                    try
+                    {
+                        var profile = await _companyService.GetCompanyProfileByIdAsync(id);
 
-                if (profile == null)
-                    return NotFound();
+                        if (profile == null)
+                            return NotFound();
 
-                return Ok(profile);
+                        return Ok(profile);
             }
-            catch (Exception ex)
+            catch(Exception ex)
             {
                 return StatusCode(500, ex.Message);
             }
@@ -121,23 +74,6 @@ namespace JOB_PORTAL_SYSTEM.Api.Admin
                 return StatusCode(500, ex.Message);
             }
         }
-
-        [HttpPost("location")]
-        public async Task<IActionResult> AddLocation(AddLocationDto dto)
-        {
-            try
-            {
-                var result = await _locationRepository
-                    .AddLocationAsync(dto);
-
-                return Ok(result);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
-        }
-
         [HttpPost("add-skills")]
         public async Task<IActionResult> AddSkills([FromBody] List<AddSkillDto> skills)
         {
@@ -203,29 +139,5 @@ namespace JOB_PORTAL_SYSTEM.Api.Admin
                 return StatusCode(500, ex.Message);
             }
         }
-
-        [HttpPost("qualification")]
-        public async Task<IActionResult> AddQualification(AddQualificationDto dto)
-        {
-            try
-            {
-                var result = await _qualificationService.AddQualificationAsync(dto);
-
-                return Ok(result);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
-        }
-
-        //[Authorize(Roles = "Admin")]
-        //[HttpPost("logout")]
-
-        //public IActionResult Logout()
-        //{
-        //    return Ok("Logged Out Successfully");
-        //}
-
     }
 }
