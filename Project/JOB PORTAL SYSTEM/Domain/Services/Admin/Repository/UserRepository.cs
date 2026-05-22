27e6d7984f7b89b61e7fb41069afe4fa7ffa1ac9
junaidksamad <junaidksamad@gmail.com>
2026-05-22 10:47:52 +0530

    h

--- Project/JOB PORTAL SYSTEM/Domain/Services/Admin/Repository/UserRepository.cs
+++ Project/JOB PORTAL SYSTEM/Domain/Services/Admin/Repository/UserRepository.cs
@@ -61,12 +61,12 @@ namespace Domain.Services.Admin.Repository
                         Id = x.Id,
 
                         FullName =
-                            x.FirstName + " " +
-                            x.LastName,
+                            x.User.FirstName + " " +
+                            x.User.LastName,
 
-                        Email = "",
+                        Email = x.User.Email,
 
-                        PhoneNumber = "",
+                        PhoneNumber = x.User.PhoneNumber,
 
                         Role = "JobProvider"
                     })
