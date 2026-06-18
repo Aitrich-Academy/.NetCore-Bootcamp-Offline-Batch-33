72dc1ff7a5bffa0a00ce34d368e0b87d9d2838db
Abijith_ps <abijithps2003@gmail.com>
2026-06-18 10:04:59 +0530

    Data Binding

---
+++ Angular/Abijith ps/Binding/workshop1/platformadmin-jobProvider/src/app/content/content.component.ts
@@ -0,0 +1,105 @@
+import { Component } from '@angular/core';
+import { Company } from '../jobproviderlist';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css']
+})
+export class ContentComponent {
+  company: Company[];
+  filteredCompany :Company[];
+  constructor() {
+    this.company = [
+      {
+      
+        img:"assets/images/companyLogo.png",
+        companyName: "Funk Inc.",
+        jobRole: "IT Department"
+      },
+      {
+      
+        img:"assets/images/companyLogo.png",
+        companyName: "Aitrich Ltd.",
+        jobRole: "IT Department"
+
+      },
+      {
+      
+        img:"assets/images/companyLogo.png",
+        companyName: "Highspeed Studios",
+        jobRole: "Creative Design Agency"
+      },
+      {
+      
+        img:"assets/images/companyLogo.png",
+        companyName: "Mosciski Inc.",
+        jobRole: "Creative Design Agency"
+      },
+      {
+      
+        img:"assets/images/companyLogo.png",
+        companyName: "Incubator Studios",
+        jobRole: "Software House"
+      },
+      {
+      
+        img:"assets/images/companyLogo.png",
+        companyName: "Naonotu Inc.",
+        jobRole: "Creative Design Agency"
+      },
+      {
+      
+        img:"assets/images/companyLogo.png",
+        companyName: "ColoColo Studios",
+        jobRole: "Internet Service Porvider"
+      },
+      {
+      
+        img:"assets/images/companyLogo.png",
+       companyName: "Funk Inc.",
+        jobRole: "IT Department"
+      },
+      {
+      
+        img:"assets/images/companyLogo.png",
+        companyName: "Mosciski Inc.",
+        jobRole: "Creative Design Agency"
+      },
+      {
+      
+        img:"assets/images/companyLogo.png",
+        companyName: "Highspeed Studios",
+        jobRole: "Creative Design Agency"
+      },
+      {
+      
+        img:"assets/images/companyLogo.png",
+        companyName: "Simonis Ltd",
+        jobRole: "Internet Service Porvider"
+      },
+      {
+      
+        img:"assets/images/companyLogo.png",
+        companyName: "Funk Inc.",
+        jobRole: "IT Department"
+      },
+    ];
+
+    this.filteredCompany = this.company;
+  }
+
+  filterJobs(event: Event): void {
+    const searchTerm = (event.target as HTMLInputElement).value;
+    this.filteredCompany = this.company.filter(company =>
+      company.companyName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      company.jobRole.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  }
+
+  isHovered: boolean = false;
+
+}
+
+
+
