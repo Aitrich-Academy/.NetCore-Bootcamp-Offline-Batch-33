ee243c0e370d5d3fa373ebf94140232822255de1
irshadCI001 <irshad.ibrahim7770@gmail.com>
2026-06-20 19:49:13 +0530

    add

--- Project/JOB PORTAL SYSTEM/JOB PORTAL SYSTEM/Api/Admin/AdminController.cs
+++ Project/JOB PORTAL SYSTEM/JOB PORTAL SYSTEM/Api/Admin/AdminController.cs
@@ -8,8 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace JOB_PORTAL_SYSTEM.Api.Admin
 {
-
-    [Authorize(Roles ="Admin")]
+    [Authorize(Roles = "Admin")]
 
     [Tags("02-Admin")]
     [Route("api/[controller]")]
